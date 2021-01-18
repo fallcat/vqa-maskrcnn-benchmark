483d01072f89ff2f5c6179e9b337abf77b2b2da6
fallcat <rachel_you@outlook.com>
2021-01-17 22:13:44 -0500

    debug proposals

--- maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
+++ maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
@@ -46,6 +46,7 @@ class GeneralizedRCNN(nn.Module):
         """
         if self.training and targets is None:
             raise ValueError("In training mode, targets should be passed")
+        print("images", len(images), images[0].shape)
         images = to_image_list(images)
         print("images", images.tensors.shape)
         features = self.backbone(images.tensors)
